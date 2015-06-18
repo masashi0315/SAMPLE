f684b74b94b28463d0cad99520d6cf3230fbbb97
inoue masashi <inoue@eiwa-giken.com>
2015-06-18 17:11:31 +0900

    初期コミット

---
+++ RandomPassword/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  RandomPassword
+//
+//  Created by macbookpro on 2015/06/18.
+//  Copyright (c) 2015年 macbookpro. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
